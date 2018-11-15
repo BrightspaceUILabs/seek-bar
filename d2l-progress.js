a5313c7ef9f4f7b74b4065bee5a76f000fc0cbf3
Quan Hui Wang <quanhui.wang@d2l.com>
2018-11-15 14:05:32 -0500

    Polymer 3 Conversion

---
+++ d2l-progress.js
@@ -0,0 +1,61 @@
+import '../../@polymer/polymer/polymer-legacy.js';
+import '../../@polymer/iron-flex-layout/iron-flex-layout-classes.js';
+import { IronRangeBehavior } from '../../@polymer/iron-range-behavior/iron-range-behavior.js';
+import '../../d2l-colors/d2l-colors.js';
+import { Polymer } from '../../@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '../../@polymer/polymer/lib/utils/html-tag.js';
+Polymer({
+  _template: html`
+		<style is="custom-style" include="iron-positioning">
+			:host {
+				display: block;
+				width: 100%;
+				position: relative;
+				overflow: hidden;
+			}
+
+			 :host([hidden]) {
+				display: none !important;
+			}
+
+			#progressContainer {
+				@apply --d2l-progress-container;
+				position: relative;
+				height: var(--d2l-progress-height, 4px);
+				background: var(--d2l-progress-container-color, var(--d2l-color-gypsum));
+			}
+
+			#primaryProgress {
+				@apply --d2l-progress-primary;
+				background: var(--d2l-progress-active-color, var(--d2l-color-celestine));
+			}
+		</style>
+
+		<div id="progressContainer">
+			<div id="primaryProgress" class="fit"></div>
+		</div>
+`,
+
+  is: 'd2l-progress',
+
+  behaviors: [
+	  IronRangeBehavior
+  ],
+
+  observers: [
+	  '_progressChanged(value, min, max)'
+  ],
+
+  _transformProgress: function(progress, ratio) {
+	  var transform = ratio + '%';
+	  progress.style.width = transform;
+  },
+
+  _progressChanged: function(value) {
+	  value = this._clampValue(value);
+
+	  var mainRatio = this._calcRatio(value) * 100;
+
+	  this._transformProgress(this.$.primaryProgress, mainRatio);
+  }
+});
